2b8d843e9b57e3fa03a4549355ea46a41834628a
camilo <milo.h@aol.com>
2017-01-28 11:20:17 -0500

    license headers

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
+/* Copyright 2017 Camilo Higuita(milohr)
+*
+* This file is part of Babe Qt Music Player.
+*
+* Babe is free software: you can redistribute it
+* and/or modify it under the terms of version 3 of the
+* GNU General Public License as published by the Free Software Foundation.
+*
+* Babe is distributed in the hope that it will be
+* useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+* Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with Babe. If not, see http://www.gnu.org/licenses/.
+*/
+
+
+
 #include "mainwindow.h"
 #include <QApplication>
 
