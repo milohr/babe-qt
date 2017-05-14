43c6fa550423db2ab93313e265bf3a0b0389d7d0
Enoque <enoquejoseneas@gmail.com>
2017-05-13 21:20:44 -0300

    removed empty spaces

--- main.cpp
+++ main.cpp
@@ -12,11 +12,7 @@
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software Foundation,
    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-
    */
-
-
-
 
 #include "mainwindow.h"
 #include <QApplication>
@@ -28,7 +24,5 @@ int main(int argc, char *argv[])
     a.setDesktopFileName("Babe");
     MainWindow w;
     w.show();
-
-
     return a.exec();
 }
