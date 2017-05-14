05fc6d02ad8a741bca5487c5bb10988ed1242fde
Enoque <enoquejoseneas@gmail.com>
2017-05-14 19:50:11 -0300

    Added some code fixes

--- youtube.h
+++ youtube.h
@@ -1,39 +1,34 @@
 #ifndef YOUTUBE_H
 #define YOUTUBE_H
-#include <QObject>
-#include <QWidget>
-#include <QProcess>
+
 #include <QByteArray>
-#include <QMovie>
 #include <QDebug>
 #include <QDirIterator>
+#include <QMovie>
+#include <QObject>
+#include <QProcess>
+#include <QWidget>
 
 #include "baeUtils.h"
-
 
 class YouTube : public QObject
 {
     Q_OBJECT
-
-
 public:
     explicit YouTube(QObject *parent = 0);
-    void fetch(QStringList ids_, QStringList urls_={});
     ~YouTube();
+    void fetch(QStringList ids_, QStringList urls_ = {});
     void searchPendingFiles();
     QStringList ids;
     QStringList urls;
 
-
 private slots:
-
     void processFinished();
-    void processFinished_totally(int state,QString id,QProcess::ExitStatus exitStatus);
+    void processFinished_totally(int state, QString id, QProcess::ExitStatus exitStatus);
 
 private:
-
-    const QString ydl="youtube-dl --metadata-from-title \"%(artist)s - %(title)s\"  --format m4a  --add-metadata";
-    const QString cachePath=BaeUtils::getYoutubeCachePath();
+    const QString ydl = "youtube-dl --metadata-from-title \"%(artist)s - %(title)s\"  --format m4a  --add-metadata";
+    const QString cachePath = BaeUtils::getYoutubeCachePath();
     const QString extensionFetchingPath = BaeUtils::getExtensionFetchingPath();
 
 signals:
